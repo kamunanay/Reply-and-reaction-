a54c1d4a2f97379109391ea3431253176a3bf9ee
Anonim <kadal.rigel@gmail.com>
2025-10-02 04:04:04 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,16 +1,2 @@
-const autoReply = {
-    "hai": "Hai juga 👋",
-    "assalamualaikum": "Waalaikumsalam 😇",
-    "ping": "Pong!",
-    "bot": "Ya saya bot WhatsApp 🤖"
-};
-
-const autoReaction = {
-    "hai": "👋",
-    "love": "❤️",
-    "ok": "👌",
-    "mantap": "🔥",
-    "bot": "🤖"
-};
-
-module.exports = { autoReply, autoReaction };
+// nanti bisa dipakai untuk nyimpen data kalau perlu
+module.exports = {};
